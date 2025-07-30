59b11a5fdb55073f1c026234ef9a8b45525acbc8
Lupu Dan Cristian <lupudancristian@gmail.com>
2025-07-30 10:03:17 +0300

    End of section 11

---
+++ client/src/app/shared/components/DateTimeInput.tsx
@@ -0,0 +1,28 @@
+    import { type FieldValues, useController, type UseControllerProps } from "react-hook-form";
+    import { DateTimePicker } from '@mui/x-date-pickers';
+     
+    type Props<T extends FieldValues> = UseControllerProps<T> & {
+      label?: string;
+    };
+     
+    export default function DateTimeInput<T extends FieldValues>(props: Props<T>) {
+      const { field, fieldState } = useController({ ...props });
+     
+      return (
+        <DateTimePicker
+          value={field.value ?? null}
+          onChange={(value) => {
+            field.onChange(value);
+          }}
+          sx={{ width: '100%' }}
+          slotProps={{
+            textField: {
+              label: props.label,
+              onBlur: field.onBlur,
+              error: !!fieldState.error,
+              helperText: fieldState.error?.message,
+            }
+          }}
+        />
+      );
+    }
